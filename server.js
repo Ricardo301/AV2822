ae284785ec388a480fcac82fa0d7e1dd2c8fc16e
ricardo301 <ricardo9251@gmail.com>
2021-11-11 20:11:36 -0300

    DB Adicionado

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+
+const db = require('./app/db/models');
+db.sequelize.sync()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
